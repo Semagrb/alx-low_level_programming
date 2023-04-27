fd3a3b8f93298d12c7029536b122d437fe16a462
Semagrb <fithisama@gmail.com>
2023-04-27 14:13:57 -0700

    super

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+int _isupport(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print square(int size);
+void print_triangle(int size);
+void print_number(int n);
