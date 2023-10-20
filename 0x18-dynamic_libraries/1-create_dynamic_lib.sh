b1f2aa88ad43ca33eb4ea30b0104ecaa408ae657
semagrb <fithisalma@gmail.com>
2023-10-20 01:24:01 -0700

    updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared *.o -o liball.so
+
+# Compile all the .c files in the current directory into a shared library.
+gcc -Wall -pedantic -Werror -Wextra -shared -fPIC -o liball.so *.c
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+  echo "Dynamic library liball.so created successfully."
+else
+  echo "Compilation failed."
+fi
